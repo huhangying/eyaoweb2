9afa3e5328bc5d3fe2f62b3e9c1daebdd53d593d
Harry Hu <harry.hu@yahoo.ca>
2020-04-30 15:59:35 -0400

    enhance login messages

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,17 +1,22 @@
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AppStoreService } from '../../my-core/store/app-store.service';
 import { AuthService } from '../../my-core/service/auth.service';
+import { tap, catchError, takeUntil } from 'rxjs/operators';
+import { Message } from '../../my-core/enum/message.enum';
+import { EMPTY, Subject } from 'rxjs';
 
 @Component({
   selector: 'ngx-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  destroy$ = new Subject<void>();
   loginForm: FormGroup;
   errorResult = false;
+  errorMessage: string;
 
   constructor(
     private fb: FormBuilder,
@@ -28,6 +33,17 @@ export class LoginComponent implements OnInit {
       userId: ['', [Validators.required, Validators.minLength(4)]],
       password: ['', [Validators.required, Validators.minLength(4)]],
     });
+    this.loginForm.valueChanges.pipe(
+      tap(() => {
+        this.errorResult = false;
+      }),
+      takeUntil(this.destroy$)
+    ).subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
   }
 
   validate(controlName: string): boolean {
@@ -36,18 +52,34 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    this.authService.login(this.userId.value, this.password.value)
-      .subscribe((doctor) => {
+    this.authService.login(this.userId.value, this.password.value).pipe(
+      tap(doctor => {
         if (doctor?._id) {
           this.appStore.updateDoctor(doctor);
           // console.log(this.appStore.state);
           this.router.navigate(['/cms/dashboard']);
         } else {
           // error
-          console.error(doctor);
-          this.errorResult = true;
+          this.setErrorMessage();
         }
-      });
+      }),
+      catchError(err => {
+        this.setErrorMessage(err.error?.return);
+        return EMPTY;
+      })
+    ).subscribe();
+  }
+
+  setErrorMessage(code?: string) {
+    this.errorResult = true;
+    if (code === 'not_registered') {
+      return Message.not_registered;
+    } else if (code === 'wrong_password') {
+      return Message.wrong_password;
+    } else {
+      return Message.login_failed;
+    }
+
   }
 
 }
--- src/app/my-core/enum/message.enum.ts
+++ src/app/my-core/enum/message.enum.ts
@@ -6,4 +6,9 @@ export const enum Message {
   defaultError = '服务器错误',
   nameExisted = '编辑的名称已经存在，请选择其它值。',
   deleteNotAllowed = '不能删除，可能是存在关联数据',
+
+  // login
+  not_registered = '用户名不存在, 请确认后重试!',
+  wrong_password = '密码不正确, 请确认后重试!',
+  login_failed = '不正确的用户名或密码, 请确认后重试!',
 }
