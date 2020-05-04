571e79301b6abbf2f499cd532b8fa8de1de13420
Harry Hu <harry.hu@yahoo.ca>
2020-05-03 20:07:43 -0400

    schedule: populate period

--- src/app/models/reservation/schedule.model.ts
+++ src/app/models/reservation/schedule.model.ts
@@ -1,9 +1,16 @@
 export interface Schedule {
   _id: string;
   doctor: string; // id
-  period: string; // id
+  // period: string; // id
+  period: Period;
   date: Date;
   limit?: number;
   created?: Date;
   apply?: boolean;
+}
+
+export interface Period {
+  name: string;
+  from: number;
+  to?: number;
 }
