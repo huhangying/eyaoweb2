3806f500e596018e4f28b5ccb632597b90a2394c
Harry Hu <harry.hu@yahoo.ca>
2020-05-04 11:25:37 -0400

    add mat chinese date/moment support

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,11 +9,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { DoctorProfileComponent } from './components/doctor-profile/doctor-profile.component';
-import { MatNativeDateModule } from '@angular/material/core';
+import { MatNativeDateModule, DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { SelectDoctorComponent } from '../cms/shared/select-doctor/select-doctor.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatBadgeModule } from '@angular/material/badge';
+import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
 
 @NgModule({
   imports: [
@@ -27,7 +28,7 @@ import { MatBadgeModule } from '@angular/material/badge';
     MatButtonModule,
     MatDialogModule,
     MatDatepickerModule,
-    MatNativeDateModule,
+    // MatNativeDateModule,
     MatTooltipModule,
     MatTabsModule,
     MatBadgeModule,
@@ -44,7 +45,7 @@ import { MatBadgeModule } from '@angular/material/badge';
     MatButtonModule,
     MatDialogModule,
     MatDatepickerModule,
-    MatNativeDateModule,
+    // MatNativeDateModule,
     MatTooltipModule,
     MatTabsModule,
     MatBadgeModule,
@@ -57,7 +58,10 @@ import { MatBadgeModule } from '@angular/material/badge';
     SelectDoctorComponent,
   ],
   providers: [
-    MatDatepickerModule
+    MatDatepickerModule,
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+    { provide: MAT_DATE_LOCALE, useValue: 'zh-cn' }, //注意 moment 是 zh-cn 和 ng 不一样
   ]
 })
 export class SharedModule { }
