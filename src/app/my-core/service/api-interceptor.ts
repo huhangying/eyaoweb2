6537783f42e64d938195fedf83db6a507e1ecb51
Harry Hu <harry.hu@yahoo.ca>
2020-05-05 11:24:07 -0400

    redirect to login when 401/403

--- src/app/my-core/service/api-interceptor.ts
+++ src/app/my-core/service/api-interceptor.ts
@@ -3,12 +3,14 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { finalize, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
 
   constructor(
     private appStore: AppStoreService,
+    private router: Router,
   ) {
   }
 
@@ -22,12 +24,16 @@ export class ApiInterceptor implements HttpInterceptor {
     }
     this.appStore.updateLoading(true);
     return next.handle(request).pipe(
-      finalize(() => {
-        this.appStore.updateLoading(false);
-      }),
       catchError((error: HttpErrorResponse) => {
         // handle error
+        // 403 => redirect to login
+        if (error.status === 403 || error.status === 401) {
+          this.router.navigate(['auth/login']);
+        }
         return throwError(error);
+      }),
+      finalize(() => {
+        this.appStore.updateLoading(false);
       }),
     );
   }
