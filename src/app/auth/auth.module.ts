916da08b6ba56aedecb1d0b2d00d43f7611b4297
Harry Hu <harry.hu@yahoo.ca>
2020-05-08 14:15:09 -0400

    minor

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
-import { NbButtonModule, NbCardModule, NbAlertModule, NbInputModule, NbCheckboxModule } from '@nebular/theme';
-import { ReactiveFormsModule } from '@angular/forms';
-
 import { AuthComponent } from './auth.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
-import { ThemeModule } from '../@theme/theme.module';
-import { DashboardModule } from '../pages/dashboard/dashboard.module';
 import { SharedModule } from '../shared/shared.module';
 
 const routes: Routes = [
@@ -21,12 +16,12 @@ const routes: Routes = [
         pathMatch: 'full',
       },
       {
-        path: '404',
-        component: NotFoundComponent,
-      },
-      {
         path: 'login',
         component: LoginComponent,
+      },
+      {
+        path: '404',
+        component: NotFoundComponent,
       },
       {
         path: '**',
