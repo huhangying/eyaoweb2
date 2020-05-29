1d8d5964202ab40f5d26c23a5fae6b77f02832cc
Harry Hu <harry.hu@yahoo.ca>
2020-05-29 13:24:35 -0400

    add api/ prefix to calls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://139.224.68.92:3000/',
-  apiUrl: 'http://192.168.87.250:3000/',
+  apiUrl: 'api/',
   // imageServer: 'http://139.224.68.92:81/',
   imageServer: 'http://192.168.87.250:3000/',
   peerPageUrl: 'http://yyl.rostensoft.com/zhaoys/',
