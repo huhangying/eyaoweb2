ff10c5e122d36306a1a46607f2c8df6d010a7477
Harry Hu <harry.hu@yahoo.ca>
2020-06-22 09:18:28 -0400

    use local server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,6 +14,7 @@ export const environment = {
   // apiUrl: 'api/',
   // apiUrl: 'http://192.168.87.35:3000/', //local
   imageServer: 'http://192.168.87.250:888/',
-  socketUrl: 'http://localhost:3000',
+  socketUrl: 'http://192.168.87.250:3000',
+  // socketUrl: 'http://timebox.i234.me/api',
   wechatServer: 'http://timebox.i234.me/wechat/'
 };
