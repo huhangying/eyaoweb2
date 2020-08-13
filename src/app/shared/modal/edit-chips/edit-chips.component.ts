f20734f12e02c7758d035853b9badc2c6d58fd90
Harry Hu <harry.hu@yahoo.ca>
2020-08-12 21:51:40 -0400

    finish diease-edit

--- src/app/shared/modal/edit-chips/edit-chips.component.ts
+++ src/app/shared/modal/edit-chips/edit-chips.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Inject, ViewChild, ElementRef, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild, ElementRef, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { BACKSLASH, ENTER } from '@angular/cdk/keycodes';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { HospitalService } from '../../../services/hospital.service';
-import { tap } from 'rxjs/operators';
+import { tap, map, distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import { AuthService } from '../../service/auth.service';
+import { FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'ngx-edit-chips',
@@ -11,14 +14,17 @@ import { tap } from 'rxjs/operators';
   styleUrls: ['./edit-chips.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EditChipsComponent implements OnInit {
+export class EditChipsComponent implements OnInit, OnDestroy {
+  destroy$ = new Subject<void>();
   readonly separatorKeysCodes: number[] = [ENTER, BACKSLASH];
   chips: string[];
   @ViewChild('tagInput', { read: ElementRef }) public tagInput: ElementRef;
-  diseases: string[];
-  tags: string[];
+  tags: string[]; // original
+  filteredTags: string[]; // unused
+  filterCtrl = new FormControl();
 
   constructor(
+    private auth: AuthService,
     public dialogRef: MatDialogRef<EditChipsComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { title: string; content: string; type?: number }, // type=1: edit-disease
     private cd: ChangeDetectorRef,
@@ -26,33 +32,60 @@ export class EditChipsComponent implements OnInit {
   ) {
     this.chips = !data.content ? [] : data.content.split('|');
     this.tags = [];
-    if (data.type) {
-      this.diseases = [
-        'sdfsd',
-        'ddddd',
-        'fffff'
-      ];
-      this.tags = [...this.diseases];
+    if (data.type === 1) { // disease edit
       this.hospitalService.getDiseases().pipe(
+        // todo: put tags in current department in front
+        map(ds => ds.map(_ => _.name)),
         tap(ds => {
-          console.log(ds);
-
+          this.tags = ds || [];
+          this.filteredTags = [...this.tags];
         })
       ).subscribe();
     }
   }
 
   ngOnInit(): void {
+
+    this.filterCtrl.valueChanges.pipe(
+      distinctUntilChanged(),
+      tap((keyword) => {
+        const unused = this.tags.filter(t => !this.isSelected(t));
+        this.filteredTags = !keyword
+          ? unused
+          : unused.filter((t) => {
+            if (this.isSelected(t)) {
+              return false;
+            }
+            return t.match(new RegExp(keyword, 'ig'))?.length;
+          });
+        this.cd.markForCheck();
+      }),
+      takeUntil(this.destroy$)
+    ).subscribe();
+  }
+
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
   }
 
   add(event: MatChipInputEvent): void {
     const input = event.input;
     const value = event.value;
 
-    // Add our fruit
+    // Add
     if ((value || '').trim()) {
       console.log('->', value);
       this.pushToChips(value);
+      if (this.data.type === 1) {
+        this.hospitalService.createDisease({
+          department: this.auth.doctor.department,
+          name: value,
+          order: 100 // defalt order set to 100
+        }).subscribe();
+      }
+      this.refreshTags();
     }
 
     // Reset the input value
@@ -69,19 +102,15 @@ export class EditChipsComponent implements OnInit {
 
     if (index >= 0) {
       this.chips.splice(index, 1);
-      this.tags.unshift(tag);
-      this.cd.markForCheck();
-
+      // this.tags.unshift(tag);
+      this.refreshTags();
     }
   }
 
   onSelected(value: string) {
-    console.log(value);
     this.pushToChips(value);
-    this.popFromTags(value);
-    this.cd.markForCheck();
-    //
-    this.diseases.push(value);
+    this.refreshTags();
+
     setTimeout(() => {
       this.tagInput.nativeElement.blur();
     }, 100);
@@ -93,10 +122,14 @@ export class EditChipsComponent implements OnInit {
     }
   }
 
-  private popFromTags(tag: string) {
-    this.tags = this.tags.filter(item => item !== tag);
+  private isSelected(tag: string) {
+    return this.chips.findIndex(_ => _ === tag) > -1;
   }
 
+  private refreshTags() {
+    this.filteredTags = this.tags.filter(t => !this.isSelected(t));
+    this.cd.markForCheck();
+  }
 
   // return model:
   // {
