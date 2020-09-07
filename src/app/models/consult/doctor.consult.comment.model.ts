fdb7e3069a107c82229c7a59fd5c7fffc974053c
Harry Hu <harry.hu@yahoo.ca>
2020-09-07 12:10:26 -0400

    add doctor-consult-comment service

---
+++ src/app/models/consult/doctor.consult.comment.model.ts
@@ -0,0 +1,16 @@
+export interface DoctorConsultComment {
+  doctor_id: string;
+  user: string; // id
+  consult: string; // id
+  consultType?: number; // helper: 0: 图文咨询； 1：电话咨询
+
+  score?: number;  // 评分
+  presetComments?: [
+    {
+      preset: string;
+      checked: boolean;
+    }
+  ];
+  comment?: string;
+  updatedAt?: Date;
+}
--- src/app/services/consult.service.ts
+++ src/app/services/consult.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../shared/service/api.service';
 import { DoctorConsult } from '../models/consult/doctor.consult.model';
+import { DoctorConsultComment } from '../models/consult/doctor.consult.comment.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +13,31 @@ export class ConsultService {
   ) { }
 
   // doctor consult
+
   getDoctorConsultByDoctorId(doctorId: string) {
     return this.api.get<DoctorConsult>('doctor-consult/' +  doctorId);
   }
 
   updateDoctorConsult(doctorId: string, data: DoctorConsult) {
     return this.api.post<DoctorConsult>('doctor-consult/' + doctorId, data);
+  }
+
+  // doctor consult comment
+
+  getAllDoctorConsultComments(doctorId: string) {
+    return this.api.get<DoctorConsultComment[]>('doctor-consult-comment/' +  doctorId);
+  }
+
+  getDoctorConsultCommentsBy(doctorId: string, from: number, size: number) {
+    return this.api.get<DoctorConsultComment[]>(`doctor-consult-comment/${doctorId}/${from}/${size}`);
+  }
+
+  addDoctorConsultComment(dcc: DoctorConsultComment) {
+    return this.api.post<DoctorConsultComment>('doctor-consult-comment', dcc);
+  }
+
+  deleteDoctorConsultCommentById(id: string) {
+    return this.api.delete('doctor-consult-comment/' + id);
   }
 
 }
