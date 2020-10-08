136f3af6d4d1d593a21d1078a1baf1e9da949df7
Harry Hu <harry.hu@yahoo.ca>
2020-10-07 22:40:00 -0400

    add dev environment

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+export const environment = {
+  production: true,
+  fontUrl: 'http://timebox.i234.me/assets/FZYTK.TTF',
+  apiUrl: 'api/',
+  imageServer: 'http://timebox.i234.me/images/',
+  socketUrl: 'http://timebox.i234.me:3000',
+  wechatServer: 'http://timebox.i234.me/wechat/'
+};
