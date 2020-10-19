f9d1052a598759222aa7ef44da262b1151442c4b
Harry Hu <harry.hu@yahoo.ca>
2020-10-19 01:47:18 -0400

    refactor doctor consult

--- src/app/shared/components/doctor-profile/doctor-consult-prices/doctor-consult-prices.component.ts
+++ src/app/shared/components/doctor-profile/doctor-consult-prices/doctor-consult-prices.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { ConsultServicePrice } from '../../../../models/consult/doctor-consult.model';
 import { DoctorService } from '../../../../services/doctor.service';
-import { Doctor } from '../../../../models/crm/doctor.model';
 import { MessageService } from '../../../service/message.service';
-import { tap } from 'rxjs/operators';
+import { distinctUntilChanged, takeUntil, tap } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'ngx-doctor-consult-prices',
   templateUrl: './doctor-consult-prices.component.html',
-  styleUrls: ['./doctor-consult-prices.component.scss']
+  styleUrls: ['./doctor-consult-prices.component.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DoctorConsultPricesComponent implements OnInit {
+export class DoctorConsultPricesComponent implements OnInit, OnDestroy {
+  destroy$ = new Subject<void>();
+  form: FormGroup;
   @Input() userId: string;
   @Input() set prices(values: ConsultServicePrice[]) {
     if (values?.length) {
@@ -18,67 +22,131 @@ export class DoctorConsultPricesComponent implements OnInit {
     }
   }
 
-  user_id: string;
-  textServiceEnabled: boolean;
-  textServicePrice: ConsultServicePrice;
-  phoneServiceEnabled: boolean;
-  phoneServicePrice: ConsultServicePrice;
-
   constructor(
     private doctorService: DoctorService,
+    private fb: FormBuilder,
     private message: MessageService,
-  ) { }
+    private cd: ChangeDetectorRef,
+  ) {
+    this.form = this.fb.group({
+      textServiceEnabled: [],
+      textServiceAmount: ['', Validators.min(0)],
+      phoneServiceEnabled: [],
+      phoneServiceAmount: ['', Validators.min(0)],
+      phoneServiceMinute: ['', Validators.min(1)]
+    });
+  }
+
+  get textServiceEnabled() { return this.form.get('textServiceEnabled'); }
+  get textServiceAmount() { return this.form.get('textServiceAmount'); }
+  get phoneServiceEnabled() { return this.form.get('phoneServiceEnabled'); }
+  get phoneServiceAmount() { return this.form.get('phoneServiceAmount'); }
+  get phoneServiceMinute() { return this.form.get('phoneServiceMinute'); }
 
   ngOnInit(): void {
+    this.textServiceEnabled.valueChanges.pipe(
+      distinctUntilChanged(),
+      tap(value => {
+        if (value) {
+          this.textServiceAmount.setValidators([Validators.required, Validators.min(0)]);
+        } else {
+          this.textServiceAmount.clearValidators();
+        }
+        this.textServiceAmount.updateValueAndValidity();
+      }),
+      takeUntil(this.destroy$)
+    ).subscribe();
+
+    this.phoneServiceEnabled.valueChanges.pipe(
+      distinctUntilChanged(),
+      tap(value => {
+        if (value) {
+          this.phoneServiceAmount.setValidators([Validators.required, Validators.min(0)]);
+          this.phoneServiceMinute.setValidators([Validators.required, Validators.min(1)]);
+        } else {
+          this.phoneServiceAmount.clearValidators();
+          this.phoneServiceMinute.clearValidators();
+        }
+        this.phoneServiceAmount.updateValueAndValidity();
+        this.phoneServiceMinute.updateValueAndValidity();
+      }),
+      takeUntil(this.destroy$)
+    ).subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
   }
 
   /////////////////////////////////// 咨询服务和价格设定
   setConsultPrices(prices: ConsultServicePrice[]) {
+    let textServiceEnabled: boolean;
+    let textServiceAmount: number;
+    let phoneServiceEnabled: boolean;
+    let phoneServiceAmount: number;
+    let phoneServiceMinute: number;
+
     // consult prices come from doctor
     // 服务和价格
-    this.textServicePrice = this.getServicePriceByType(prices, 0);
-    this.textServiceEnabled = !!this.textServicePrice;
-    if (!this.textServiceEnabled) {
-      // 设定初始值
-      this.textServicePrice = {
-        type: 0,
-        amount: 0,
-        unit_count: 1
-      };
+    const textService = this.getServicePriceByType(prices, 0);
+    if (textService && textService.amount >= 0) {
+      textServiceEnabled = true;
+      textServiceAmount = textService.amount;
+    } else {
+      textServiceEnabled = false;
     }
-    this.phoneServicePrice = this.getServicePriceByType(prices, 1);
-    this.phoneServiceEnabled = !!this.phoneServicePrice;
-    if (!this.phoneServiceEnabled) {
-      // 设定初始值
-      this.phoneServicePrice = {
-        type: 1,
-        amount: 0,
-        unit_count: 20
-      };
+
+    const phoneService = this.getServicePriceByType(prices, 1);
+    if (phoneService && phoneService.amount >= 0 && phoneService.unit_count > 0) {
+      phoneServiceEnabled = true;
+      phoneServiceAmount = phoneService.amount;
+      phoneServiceMinute = phoneService.unit_count;
+    } else {
+      phoneServiceEnabled = false;
     }
-    // this.cd.markForCheck();
 
+    this.form.patchValue({
+      textServiceEnabled,
+      textServiceAmount,
+      phoneServiceEnabled,
+      phoneServiceAmount,
+      phoneServiceMinute
+    });
 
+    this.cd.markForCheck();
   }
 
   private getServicePriceByType(servicePrices: ConsultServicePrice[], type: number) {
-    if (!servicePrices?.length) return null;
-    return servicePrices.find(sp => sp.type === type);
+    if (!servicePrices || servicePrices.length < 1) {
+      return null;
+    } else {
+      return servicePrices.find(sp => sp.type === type);
+    }
   }
 
   saveServicePrices() {
     const servicePrices = [];
-    if (this.textServiceEnabled) {
-      servicePrices.push(this.textServicePrice);
+    if (this.textServiceEnabled.value) {
+      servicePrices.push({
+        type: 0,
+        amount: this.textServiceAmount.value,
+        unit_count: 1
+      });
     }
-    if (this.phoneServiceEnabled) {
-      servicePrices.push(this.phoneServicePrice);
+    if (this.phoneServiceEnabled.value) {
+      servicePrices.push({
+        type: 1,
+        amount: this.phoneServiceAmount.value,
+        unit_count: this.phoneServiceMinute.value
+      });
     }
     // save
     this.doctorService.updateDoctor({ user_id: this.userId, prices: servicePrices }).pipe(
       tap(result => {
         if (result) {
           this.message.updateSuccess();
+          this.cd.markForCheck();
         }
       })
     ).subscribe();
