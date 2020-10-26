4263c5f2001f308cfacf623927db3fb8b134054d
Harry Hu <harry.hu@yahoo.ca>
2020-10-26 00:36:32 -0400

    finish cms consult-comments

--- src/app/cms/consult/consult-doctor-rating/doctor-comment-details/doctor-comment-details.component.ts
+++ src/app/cms/consult/consult-doctor-rating/doctor-comment-details/doctor-comment-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit, Optional, SkipSelf } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DoctorConsultComment } from '../../../../models/consult/doctor-consult-comment.model';
+import * as rater from 'rater-js';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'ngx-doctor-comment-details',
@@ -8,13 +10,44 @@ import { DoctorConsultComment } from '../../../../models/consult/doctor-consult-
   styleUrls: ['./doctor-comment-details.component.scss']
 })
 export class DoctorCommentDetailsComponent implements OnInit {
+  form: FormGroup;
 
   constructor(
     public dialogRef: MatDialogRef<DoctorCommentDetailsComponent>,
     @Inject(MAT_DIALOG_DATA) @Optional() @SkipSelf() public data: DoctorConsultComment,
-  ) { }
+    private fb: FormBuilder,
+  ) {
+    this.form = this.fb.group({
+      preset1: [''],
+      preset2: [''],
+      preset3: [''],
+      preset4: [''],
+    });
+   }
 
   ngOnInit(): void {
+    this.dialogRef.updateSize('50%');
+
+    const doctorRater = rater.default({
+      element: document.querySelector('#rater'),
+      starSize: 25,
+    });
+    doctorRater.setRating(this.data.score);
+    doctorRater.disable();
+
+    this.populateForm(this.data);
+  }
+
+  populateForm(comment: DoctorConsultComment) {
+    if (comment) {
+      this.form.patchValue({
+        preset1: comment.presetComments?.find(_ => _.type === 1).checked,
+        preset2: comment.presetComments?.find(_ => _.type === 2).checked,
+        preset3: comment.presetComments?.find(_ => _.type === 3).checked,
+        preset4: comment.presetComments?.find(_ => _.type === 4).checked,
+      });
+      this.form.disable();
+    }
   }
 
 }
