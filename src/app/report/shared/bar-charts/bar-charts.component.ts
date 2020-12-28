b5c477ae6d4cf8afe5e43a0792788321e87a7d6c
Harry Hu <harry.hu@yahoo.ca>
2020-12-27 20:53:45 -0500

    add bar chart report template

--- src/app/report/shared/bar-charts/bar-charts.component.ts
+++ src/app/report/shared/bar-charts/bar-charts.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Optional, SkipSelf } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ChartItem } from '../../models/report-search.model';
 
 @Component({
   selector: 'ngx-bar-charts',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bar-charts.component.scss']
 })
 export class BarChartsComponent implements OnInit {
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#7aa3e5', '#a8385d', '#aae3f5', '#CFC0BB']
+  };
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<BarChartsComponent>,
+    @Inject(MAT_DIALOG_DATA) @Optional() @SkipSelf() public data: {
+      title: string;
+      chartData: ChartItem[];
+      xLabel: string;
+      yLabel: string;
+    },
+  ) {
+    console.log(data.chartData);
+  }
 
   ngOnInit(): void {
+    this.dialogRef.updateSize('90%');
   }
 
 }
