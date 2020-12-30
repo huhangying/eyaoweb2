64f952b7210a71f1b41e47fc44f04271bbbf23fa
Harry Hu <harry.hu@yahoo.ca>
2020-12-30 00:27:18 -0500

    chore

--- src/app/cms/reservation/reservation.module.ts
+++ src/app/cms/reservation/reservation.module.ts
@@ -6,7 +6,6 @@ import { ScheduleEditComponent } from './schedule/schedule-edit/schedule-edit.co
 import { ScheduleBatEditComponent } from './schedule/schedule-bat-edit/schedule-bat-edit.component';
 import { ScheduleBatDeleteComponent } from './schedule/schedule-bat-delete/schedule-bat-delete.component';
 import { BookingForwardDoctorComponent } from './booking/booking-forward-doctor/booking-forward-doctor.component';
-import { PeriodComponent } from './period/period.component';
 import { PeriodEditComponent } from './period/period-edit/period-edit.component';
 
 @NgModule({
