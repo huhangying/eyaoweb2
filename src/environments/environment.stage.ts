844827585b68fdce5b50182eb9c203366df70c13
Harry Hu <harry.hu@yahoo.ca>
2021-01-13 17:06:44 -0500

    add stage env

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+export const environment = {
+  production: true,
+  fontUrl: 'http://www.zhaoyaoshi885.com/assets/FZYTK.TTF',
+  apiUrl: 'api/',
+  imageServer: 'http://www.zhaoyaoshi885.com/images/',
+  socketUrl: 'http://www.zhaoyaoshi885.com:3000',
+  wechatServer: 'http://www.zhaoyaoshi885.com/wechat/'
+};
